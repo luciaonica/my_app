7fb7983eb28ad8cbbb186de59d34163df9ac5184
lucia <lucia12onica@gmail.com>
2023-05-25 09:39:03 -0500

    update my_app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
